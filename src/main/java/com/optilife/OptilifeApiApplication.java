acb7a7ac460af7ddec226573a8c5a2ff6f264c78
Alain Lanfranko <alain.lanfranko2808@gmail.com>
2024-09-13 11:01:27 -0500

    Hola mundo

---
+++ src/main/java/com/optilife/OptilifeApiApplication.java
@@ -0,0 +1,13 @@
+package com.optilife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptilifeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptilifeApiApplication.class, args);
+    }
+
+}
